51cb5069d89701ac516a475fb7a67010001a844d
Sanchez Emir <emir2104@gmail.com>
2022-03-03 17:03:02 -0500

    first commit

---
+++ src/main/java/com/nttdata/bootcointransaction/BootcoinTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcointransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcoinTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcoinTransactionApplication.class, args);
+	}
+
+}
